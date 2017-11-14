19c301bee143661f23483d608e315ad6ae79c159
Kurisu Brooks <kurisubrooks@gmail.com>
2017-11-15 00:00:20 +1100

    Add GTK+ Inspector Enabler

--- develop.sh
+++ develop.sh
@@ -1,2 +1,6 @@
 # Install Dev Libs
+echo "Installing Pre-requisites"
 sudo apt install libgtk-3-dev git build-essential -y
+
+echo "Enabling GTK+ Inspector"
+gsettings set org.gtk.Settings.Debug enable-inspector-keybinding true
